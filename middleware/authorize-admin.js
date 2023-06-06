c544cfee8f458144cc22c81a126ec5633247b2f3
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-06-06 21:56:35 +0700

    add authorize admin

---
+++ middleware/authorize-admin.js
@@ -0,0 +1,15 @@
+const authorizeAdmin = (req, res, next) => {
+  User.findById(req.userId)
+    .then(user => {
+      if (user.role === 'admin') {
+        next();
+      } else {
+        res.status(403).json({ error: 'Forbidden' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'Failed to authorize' });
+    });
+};
+
+module.exports = authorizeAdmin;
