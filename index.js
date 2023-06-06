1c52508ea07554b5ae3b4f724bb0a83629488ca9
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-06-06 21:13:29 +0700

    update db connection & install nodemon

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Route utama
 app.get('/', (req, res) => {
   res.send('Holaaa, Selamat datang!');
 });
 
+connectDB();
 
 // Jalankan server
 app.listen(port, () => {
