2c39e6a79b43378ad7256b4461d40ab7045a759a
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-06-14 18:23:27 +0700

    update middleware

--- routes/admin/admin-kelas-offline-routes.js
+++ routes/admin/admin-kelas-offline-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const auth = require('../../middleware/verify-token');
+const authorizeAdmin = require('../../middleware/authorize-admin')
 require('dotenv').config();
 
 const router = express.Router();
@@ -12,14 +13,14 @@ const {
   deleteClassById 
 } = require('../../controllers/Kelas-Offline-controller');
 
-router.post('/api/admin-kelas-offline', auth, createOfflineClass);
+router.post('/api/admin-kelas-offline', auth, authorizeAdmin, createOfflineClass);
 
-router.get('/api/admin-kelas-offline', auth, getClasses);
+router.get('/api/admin-kelas-offline', auth, authorizeAdmin, getClasses);
 
-router.get('/api/admin-kelas-offline/:id', auth, getClassById);
+router.get('/api/admin-kelas-offline/:id', auth, authorizeAdmin, getClassById);
 
-router.put('/api/admin-kelas-offline/update/:id', auth, updateClassById);
+router.put('/api/admin-kelas-offline/update/:id', auth, authorizeAdmin, updateClassById);
 
-router.delete('/api/admin-kelas-offline/delete/:id', auth, deleteClassById);
+router.delete('/api/admin-kelas-offline/delete/:id', auth, authorizeAdmin, deleteClassById);
 
 module.exports = router;
--- routes/admin/admin-kelas-online-routes.js
+++ routes/admin/admin-kelas-online-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const auth = require('../../middleware/verify-token');
+const authorizeAdmin = require('../../middleware/authorize-admin')
 require('dotenv').config();
 
 const router = express.Router();
@@ -12,14 +13,14 @@ const {
   deleteClassById 
 } = require('../../controllers/Kelas-Online-controller');
 
-router.post('/api/admin-kelas-online', auth, createOnlineClass);
+router.post('/api/admin-kelas-online', auth, authorizeAdmin, createOnlineClass);
 
-router.get('/api/admin-kelas-online', auth, getClasses);
+router.get('/api/admin-kelas-online', auth, authorizeAdmin, getClasses);
 
-router.get('/api/admin-kelas-online/:id', auth, getClassById);
+router.get('/api/admin-kelas-online/:id', auth, authorizeAdmin, getClassById);
 
-router.put('/api/admin-kelas-online/update/:id', auth, updateClassById);
+router.put('/api/admin-kelas-online/update/:id', auth, authorizeAdmin, updateClassById);
 
-router.delete('/api/admin-kelas-online/delete/:id', auth, deleteClassById);
+router.delete('/api/admin-kelas-online/delete/:id', auth, authorizeAdmin, deleteClassById);
 
 module.exports = router;
