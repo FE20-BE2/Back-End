10bba96f915a66d543289b1252602a6445100ea9
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-06-14 22:08:42 +0700

    update mentor

--- routes/admin/mentor-kelas-routers.js
+++ routes/admin/mentor-kelas-routers.js
@@ -18,7 +18,7 @@ const {
 
 router.post('/api/mentor',  auth, authorizeAdmin, upload.single('mentorImg'), createMentor);
 
-router.post('/api/mentor',  auth, authorizeAdmin, upload.single('mentorImg'), updateMentor);
+router.update('/api/mentor/update',  auth, authorizeAdmin, upload.single('mentorImg'), updateMentor);
 
 router.get('/api/mentor',  auth, authorizeAdmin, getAllMentors);
 
